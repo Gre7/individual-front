856d9527a390cc7f6830b157f02611b36f5c38da
Danil-Malkin-NN <danilmalkin2015@gmail.com>
2022-09-07 21:13:21 +0300

    init commit

---
+++ src/main/kotlin/ru/nino/individual/IndividualApplication.kt
@@ -0,0 +1,11 @@
+package ru.nino.individual
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IndividualApplication
+
+fun main(args: Array<String>) {
+    runApplication<IndividualApplication>(*args)
+}
