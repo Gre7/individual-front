af77167ad66f3ac98a2227ee6deab400c549b2a7
Efim Trofimov <grek521@mail.ru>
2022-09-15 20:32:26 +0300

    feat/add front with cra

---
+++ front/config-overrides.js
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+const { alias, configPaths } = require('react-app-rewire-alias');
+
+module.exports = (config) => {
+  const overrider = alias(configPaths('./tsconfig.paths.json'));
+  return overrider(config);
+};
